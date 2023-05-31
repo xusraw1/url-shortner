760dddfeedf147c5a84887aeebda2f2c17670b41
Xusrav <xusrav200329@gmail.com>
2023-05-31 18:50:00 +0500

    Modify index view, create index.html, runserver and chek how its work

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from uuid import uuid4
+from .models import Url
 
-# Create your views here.
 
 def index(request):
-    pass
+    url = request.POST.get('url')
+    if url:
+        if request.method == 'POST':
+            uid = str(uuid4())[:5]
+            new_url = Url.objects.create(link=url, uuid=uid)
+            new_url.save()
+            context = {
+                'new_url': new_url,
+            }
+            return render(request, 'index.html', context)
+    return render(request, 'index.html')

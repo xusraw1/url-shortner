359122d8bc47d2902865941bb20ed79c7acc2b4c
Xusrav <xusrav200329@gmail.com>
2023-05-31 14:43:01 +0500

    Create a function for main page and create urls.py in shortner app, than i connect my created function with url, in last i create model for saving urls

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Url(models.Model):
+    link = models.CharField(max_length=1000)
+    uuid = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.uuid)
